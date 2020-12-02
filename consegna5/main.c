0e5fcfca47106ee513a5425ec709b8687d97a992
Andrea Bianchi <bianan96@gmail.com>
2020-12-03 00:30:57 +0100

    tested open an write file

--- consegna5/main.c
+++ consegna5/main.c
@@ -5,12 +5,17 @@
 int main(int argc, char *argv[]) {
   // Apro il file input in read
   FILE *assem;
-  assem = fopen(argv[1], 'r');
-
-  
+  assem = fopen(argv[1], "r");
+  int len = strlen(argv[1]);
+  char outfile[len];
+  strcpy(outfile, argv[1]);
+  strcat(outfile, "hack");
+  FILE *output;
+  output = fopen(outfile, "w");
 
 
   fclose(assem);
+  fclose(output);
 
   return 0;
 }
