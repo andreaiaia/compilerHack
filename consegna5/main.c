f8e202e37d2d36a19e06c63c87ddd980e7138600
Andrea Bianchi <bianan96@gmail.com>
2020-12-05 13:17:48 +0100

    finishe c instructions

--- consegna5/main.c
+++ consegna5/main.c
@@ -52,10 +52,13 @@ void c_instruction(char riga[], FILE *output) {
   switch (riga[i+1])
   {
   case '0':
-    codifica[3] = '1';
     codifica[4] = '1';
     codifica[6] = '1'; 
     codifica[8] = '1';
+    break;
+  
+  case '+':
+    for (int j = 4; j < 10; j++) codifica[j] = '1';
     break;
 
   case 'D':
@@ -68,9 +71,9 @@ void c_instruction(char riga[], FILE *output) {
         codifica[9] = '1';
       }
     }
-    else if (riga[i + 3] == 'A' || riga[i + 3] == 'M')
+    else if (riga[i+3] == 'A' || riga[i+3] == 'M')
     {
-      if (riga[i + 3] == 'M') codifica[3] = '1';
+      if (riga[i+3] == 'M') codifica[3] = '1';
       if (riga[i+2] == '+') codifica[8] = '1';
       else if (riga[i+2] == '-') {
         codifica[5] = '1';
@@ -83,17 +86,59 @@ void c_instruction(char riga[], FILE *output) {
       }
     }
     else {
-      codifica[3] = '1';
       codifica[6] = '1';
       codifica[7] = '1';
     }
     break;
   
   case '-':
-    // RICOMINCIARE DA QUI
+    codifica[8] = '1';
+    if (riga[i+2] == '1') {
+      codifica[4] = '1';
+      codifica[5] = '1';
+      codifica[6] = '1';
+    } else if (riga[i+2] == 'D') {
+      codifica[6] = '1';
+      codifica[7] = '1';
+      codifica[9] = '1';
+    } else {
+      if (riga[i+2] == 'M') codifica[3] = '1';
+      codifica[4] = '1';
+      codifica[5] = '1';
+      codifica[9] = '1';
+    }
     break;
 
-  default:
+  case '!': 
+    codifica[9] = '1';
+    if (riga[i+2] == 'D') {
+      codifica[6] = '1';
+      codifica[7] = '1';
+    } else {
+      if (riga[i+2] == 'M') codifica[3] = '1';
+      codifica[4] = '1';
+      codifica[5] = '1';
+    }
+    break;
+
+  default: // A/M
+    if (riga[i+1] == 'M') codifica[3] = '1';
+    if (riga[i+3] == '1') {
+      codifica[4] = '1';
+      codifica[5] = '1';
+      codifica[8] = '1';
+      if (riga[i+2] == '+') {
+        codifica[7] = '1';
+        codifica[9] = '1';
+      }
+    } else if (riga[i+3] == 'D') {
+      codifica[7] = '1';
+      codifica[8] = '1';
+      codifica[9] = '1';
+    } else {
+      codifica[4] = '1';
+      codifica[5] = '1';
+    }
     break;
   }
 
--- consegna5/test.asm
+++ consegna5/test.asm
@@ -1,13 +1,36 @@
 @22
   @31
-
 @1
-// commento
-ciao
-M = 0
-D=0
-MD = 0
-A=1
-AM = 1
-AD=0
-AMD=1
+// in tutti questi test a = 0
+M=0
+D=+1 
+MD=-1
+A=D
+AM=A
+AD=!D
+AMD=!A
+
+M=-D
+D=-A 
+MD=D+1
+A=A+1
+AM=D-1
+AD=A-1
+AMD=D+A
+
+M=D-A
+D=A-D
+MD=D&A
+A=D|A
+// Da qui in poi a = 1
+AM=M
+AD=!M
+AMD=-M
+
+M=M+1
+D=M-1
+MD=D+M
+A=D-M
+AM=M-D
+AD=D&M
+AMD=D|M
